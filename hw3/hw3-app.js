cd80f07b7b762d202e0766fea38e2a5c8f4dbed3
ogzhnozcn13 <o.ozcan.91@gmail.com>
2022-12-20 19:57:47 +0300

    20 Aralık

---
+++ hw3/hw3-app.js
@@ -0,0 +1,9 @@
+//? ODEV1: Console'dan girilen sayilarin ortalamasini bulan programi yaziniz. Program ilk olarak kullanicidan kac tane sayi gireceginin almali bu bilgi isiginda girilen sayilarin ortalamasini hesaplayarak yazdirmalidir. -----------------------------------------------------------------------------------------------------------------
+
+    let num = +prompt("Kaç tane sayı gireceksiniz:");
+
+    let numbers = +prompt(`Sayıları aralarında virgül olacak şekilde giriniz:`);
+
+    for(let num = 2; numbers.lenght = num; )
+        ort = ort
+
